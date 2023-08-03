ae4faf69cbc6c8ee42f7bd576d7219afd94427aa
marwamelfeky <marwamelfeky@gmail.com>
2023-08-03 15:27:55 +0300

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Marwa Elfeky on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
